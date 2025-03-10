4ca01dc780ec09ca004b9d0bfed013073f7c54ad
juan camilo <camilo.mro@gmail.com>
2025-03-10 16:45:10 +0000

    add logic for closing and opening menu in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,20 @@ import {getImageUrl} from "../../utils";
 
 
 function Navbar(){
+  const [menuOpen, setMenuOpen] = useState(false);
   
   return(
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">Homepage</a>
       <div className={styles.menu}>
         {/* /assets/ can't be att to getImageUlr function NSW */}
-        <img className={styles.menuBtn} src={getImageUrl('/assets/nav/menuIcon.png')}alt="menu-button" />
-        <ul className={styles.menuItems}>
+        <img className={styles.menuBtn} src={menuOpen
+        ? getImageUrl('/assets/nav/menuIcon.png')
+        : getImageUrl('/assets/nav/closeIcon.png')}
+        alt="menu-button"
+        onClick={()=> setMenuOpen(!menuOpen)} />
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+        onClick={()=> setMenuOpen(false)}>
           <li><a href="#about">About</a></li>
           <li><a href="#experience">Experience</a></li>
           <li><a href="#projects">Projects</a></li>
