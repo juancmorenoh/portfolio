91b250d020c0d28bfcac146b206da5fe2ed6c249
juan camilo <camilo.mro@gmail.com>
2025-03-11 12:44:17 +0000

    Add projectCard component

---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from './ProjectCard.module.css';
+import { getImageUrl } from '../../utils';
+function ProjectCard({project}) {
+  /*
+  project = props.project 
+  */
+  return (
+    <div className={styles.container}>
+      <img src={getImageUrl(`/assets/${project.imageSrc}`)} alt={`image-${project.title}`} className={styles.image}/>
+      <h3 className={styles.title}>{project.title}</h3>
+      <p className={styles.description}>{project.description}</p>
+      <ul className={styles.tools}>
+        {
+          project.tools.map((tool,index) =>{
+            return(
+              <li className={styles.tool} key={index}>{tool}</li>
+            );
+          })
+        }
+      </ul>
+      <div className={styles.links}>
+        <a className={styles.link} href={project.demo}>Try it</a>
+        <a className={styles.link} href={project.source}>Source</a>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCard
