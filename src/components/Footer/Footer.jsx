d14bf98c87100787104a0ed8d6ee69cb64fb291c
juan camilo <camilo.mro@gmail.com>
2025-03-12 09:36:24 +0000

    -Implement Footer component with references and contact -Add styling to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { getImageUrl } from '../../utils';
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer id='footer' className={styles.container}>
+      <div className={styles.linkContainer}>
+        <div className={styles.text}>
+          <h2>Contact</h2>
+          <p>Feel free to reach out</p>
+        </div>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <img src={getImageUrl("/assets/footer/gmail.png")} alt="gmail icon" />
+            <a href="mailto:camilo.mro@gmail.com">camilo.mro@gmail.com</a>
+          </li>
+          <li className={styles.link}>
+            <img src={getImageUrl("/assets/footer/linkedin.png")} alt="linkedIn icon" />
+            <a href="https://www.linkedin.com/in/juancmorenoh/" target="_blank">linkedin.com/juancmorenoh</a>
+          </li>
+        </ul>
+      </div>
       {/* References */}
-      <a href="https://www.flaticon.com/free-icons/young" title="young icons">Young icons created by Freepik - Flaticon</a>
-      <a href="https://www.flaticon.com/free-icons/miscellaneous" title="miscellaneous icons">Miscellaneous icons created by Mayor Icons - Flaticon</a>
+      <div className={styles.references}>
+        <a href="https://www.flaticon.com/free-icons/miscellaneous" title="miscellaneous icons" target="_blank">Miscellaneous icons created by Mayor Icons - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/young" title="young icons" target="_blank">Young icons created by Freepik - Flaticon</a>
+      </div>  
     </footer>
   )
 }
