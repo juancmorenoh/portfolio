20d04a18451d843e73f03762e71e46f575b466b5
juan camilo <camilo.mro@gmail.com>
2025-03-12 09:32:52 +0000

    -Modify about text -Modify general font-family

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import styles from './App.module.css'
 
 import Navbar from './components/Navbar/Navbar.jsx'
 import Intro from './components/Intro/Intro.jsx'
-import Footer from './components/Footer/Footer.jsx'
 import About from './components/About/About.jsx'
 import Tools from './components/Tools/Tools.jsx'
 import Experience from './components/Experience/Experience.jsx'
 import Projects from './components/Projects/Projects.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
 function App() {
   
 
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,21 +13,21 @@ function About() {
             <img src={getImageUrl("/assets/about/website.png")} alt="website" className={styles.aboutIcon}/>
             <div className={styles.aboutItemText}>
               <h3>FrontEnd</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </p>
+              <p>I have a solid foundation in front-end development, with experience working with HTML, CSS, and JavaScript. I have built responsive, user-friendly interfaces and I am passionate about creating seamless and interactive web experiences. I am eager to apply my front-end skills to new projects and continue learning in the field. </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("/assets/about/server.png")} alt="server" className={styles.aboutIcon}/>
             <div className={styles.aboutItemText}>
               <h3>BackEnd</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </p>
+              <p>Along with my front-end skills, I have also gained experience in back-end development. I have worked with Python and Django to build server-side applications and APIs. I am familiar with setting up databases, handling server requests, and ensuring smooth communication between the front-end and back-end. </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("/assets/about/education.png")} alt="university hat" className={styles.aboutIcon}/>
             <div className={styles.aboutItemText}>
               <h3>Graduate</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </p>
+              <p>I earned a Bachelor's degree in Computer Science, where I gained a solid understanding of key concepts like data structures and algorithms. My course included modules in PHP, SQL, R and Java, which helped me develop a well-rounded skill set in both front-end and back-end technologies.</p>
             </div>
           </li>
         </ul>
